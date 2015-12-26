d6c11a8ce3a1bb87818c5f6fcb0072407b664f9a
Cristina <cristina.luis@gmail.com>
2015-12-25 22:05:38 -0500

    more function admin and recipe models

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -3,19 +3,30 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, Category
 
 
-
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name', )}
 
 
-# TODO: save multiple ingredients at once in the admin form
 class IngredientAdmin(admin.ModelAdmin):
     # TODO: use djfractions for quantity field
     #fields = ('name', 'recipe', 'order')
     list_display = ('name', 'recipe', 'place')
 
 
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 3
+    fields = ['quantity', 'units', 'name', 'place']
+
+    def get_extra(self, request, obj=None, **kwargs):
+        '''Make it so no extra lines show if object already exists.'''
+        if obj:
+            return 0
+        return self.extra
+
 class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'book_page']
+    inlines = [IngredientInline]
     prepopulated_fields = {'slug':('title', )}
 
 
--- cookbook/models.py
+++ cookbook/models.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import slugify
 
 class Tag(models.Model):
     # TODO: tags for different things?
-    pass
+    name = models.CharField(max_length=128, unique=True, default='')
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True, default='')
@@ -22,6 +22,8 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     title = models.CharField(max_length=128)
+    book_page = models.IntegerField()
+    alt_title = models.CharField(max_length=128, blank=True, null=True)
     slug = models.SlugField(unique=True)
     category = models.ForeignKey(Category)
     serves = models.IntegerField(blank=True, null=True)
@@ -33,9 +35,9 @@ class Recipe(models.Model):
         return self.title
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
     recipe = models.ForeignKey(Recipe)
-    # TODO: use latest djfractions with DecimalFractionField
+    # TODO: use latest djfractions with DecimalFractionField?
     quantity = models.DecimalField(null=True, blank=True, 
                                    max_digits=10, 
                                    decimal_places=5)
@@ -47,16 +49,30 @@ class Ingredient(models.Model):
         ('lb', 'pound'),
         ('dash', 'dash',),
         ('can', 'can'),
-        ('qt', 'quart')
+        ('qt', 'quart'),
+        ('g', 'grams'),
+        ('ml', 'millileters'),
+        ('l', 'liters')
         )
 
     units = models.CharField(max_length=40, 
                              choices=units_choices,
                              null=True, blank=True)
+
     place = models.IntegerField(default=1)
 
+    def save(self, force_insert=False, force_update=False):
+        if self.place == "":
+            existing_places = Ingredient.objects.filter(recipe=self.recipe)\
+                                                .order_by('-place')
+            self.code = existing_places[0].place + 1
+        super(Ingredient, self).save(force_insert, force_update)
+
     def __str__(self):
-        return self.name
+        quantity = '{:.2f}'.format(self.quantity) if self.quantity else ''
+        units = self.units if self.units else ''
+        # TODO: pretty repr with proper fraction quantities
+        return '{} {} {}'.format(quantity, units, self.name)
 
     class Meta:
         #unique_together = 
